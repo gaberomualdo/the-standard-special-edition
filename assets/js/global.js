9e214e824c145d82248e95b027cb5e3351506fd2
xtrp <36894883+xtrp@users.noreply.github.com>
2020-04-06 12:20:11 +0100

    add loader

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,4 +1,9 @@
 // code for this function was taken from user etham on https://stackoverflow.com/questions/36532307/rem-px-in-javascript
-const remToPx = rem => {
+const remToPx = (rem) => {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 };
+
+// remove loading from body when loaded
+setTimeout(() => {
+    document.querySelector('body').classList.remove('loading');
+}, 250);
